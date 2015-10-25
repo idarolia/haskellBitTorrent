462285d43deebb3ee231c52bf52a1f17cc286042
Ishan Darolia <ishandarolia@gmail.com>
2015-10-25 22:15:59 +0530

    hashcalc

--- main.hs
+++ main.hs
@@ -5,14 +5,14 @@ import Data.Map(Map)
 import Text.ParserCombinators.Parsec
 import Data.BEncode
 import Data.ByteString.Lazy as B
+import Crypto.Hash.SHA1
 
 import System.IO
 import System.IO (hFlush, stdout)
 
 main = do
     --filename <- getLine
-    --let filename = "newfile"
-    let filename = "file"
+    let filename = "newfile"
     
     inpData <- B.readFile filename 
     let contents = bRead inpData
@@ -26,5 +26,15 @@ main = do
     let info = maybeDict2Dict infoDict
     
     let initLeft = findInitLeft info
-    print initLeft
 
+    let pieceLen = readDict info "piece length"
+    let pieceLength = maybeInt2Int pieceLen
+
+    let piece = readDict info "pieces"
+    let pieces = maybeString2String piece
+    let numPieces = (B.length pieces) `div` 20
+
+    let infoBencode = deparse (BDict info)
+    --print infoBencode
+    let infoHash = B.fromStrict $ hashlazy (infoBencode)
+    print infoHash
